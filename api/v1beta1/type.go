fd42a2bda57a6073295fc2f5145914fb5cdff891
sp-yduck <sudo20.t.ab@gmail.com>
2023-06-04 15:11:33 +0900

    add api/type

--- api/v1beta1/type.go
+++ api/v1beta1/type.go
@@ -1,5 +1,9 @@
 package v1beta1
 
+import (
+	"github.com/sp-yduck/proxmox/pkg/service/node/vm"
+)
+
 // ObjectReference is a reference to another Kubernetes object instance.
 type ObjectReference struct {
 	// Namespace of the referent.
@@ -15,28 +19,7 @@ type ObjectReference struct {
 type InstanceStatus string
 
 var (
-	// InstanceStatusProvisioning is the string representing an instance in a provisioning state.
-	InstanceStatusProvisioning = InstanceStatus("PROVISIONING")
-
-	// InstanceStatusRunning is the string representing an instance in a pending state.
-	InstanceStatusRunning = InstanceStatus("RUNNING")
-
-	// InstanceStatusStopped is the string representing an instance
-	// that has been stopped and can be restarted.
-	InstanceStatusStopped = InstanceStatus("STOPPED")
-
-	// InstanceStatusStopping is the string representing an instance
-	// that is in the process of being stopped and can be restarted.
-	InstanceStatusStopping = InstanceStatus("STOPPING")
-
-	// InstanceStatusSuspended is the string representing an instance
-	// that is suspended.
-	InstanceStatusSuspended = InstanceStatus("SUSPENDED")
-
-	// InstanceStatusSuspending is the string representing an instance
-	// that is in the process of being suspended.
-	InstanceStatusSuspending = InstanceStatus("SUSPENDING")
-
-	// InstanceStatusTerminated is the string representing an instance that has been terminated.
-	InstanceStatusTerminated = InstanceStatus("TERMINATED")
+	InstanceStatusPaused  = InstanceStatus(vm.ProcessStatusPaused)
+	InstanceStatusRunning = InstanceStatus(vm.ProcessStatusRunning)
+	InstanceStatusStopped = InstanceStatus(vm.ProcessStatusStopped)
 )
